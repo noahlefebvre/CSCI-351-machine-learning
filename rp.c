81ad51ba7e0a661b4196efd5aa47d9309ff9d888
Jeremy Iverson <jiverson002@csbsju.edu>
2020-10-28 12:18:12 -0500

    Echoes the contents of a file
    This program will echo the contents of a file, whose path is given as the only
    argument to the executable. The contents of the file are assumed to be stored in
    a matrix-like format. This version will first store the data in parallel arrays,
    before output it.

--- rp.c
+++ rp.c
@@ -33,7 +33,6 @@ int
 main(int argc, char * argv[])
 {
   size_t n, m;
-  double v;
 
   /* Validate command line arguments. */
   assert(2 == argc);
@@ -48,21 +47,46 @@ main(int argc, char * argv[])
   FILE * const fp = fopen(fn, "r");
   assert(fp);
 
-  /* Echo file. */
+  /* Read file. */
   fscanf(fp, "%zu %zu", &n, &m);
-  printf("%zu %zu\n", n, m);
+
+  /* Allocate memory. */
+  double * const movie1 = malloc(n * sizeof(*movie1));
+  double * const movie2 = malloc(n * sizeof(*movie2));
+  double * const movie3 = malloc(n * sizeof(*movie3));
+  double * const movie4 = malloc(n * sizeof(*movie4));
+  double * const movie5 = malloc(n * sizeof(*movie5));
+
+  /* Check for success. */
+  assert(movie1);
+  assert(movie2);
+  assert(movie3);
+  assert(movie4);
+  assert(movie5);
 
   for (size_t i = 0; i < n; i++) {
-    for (size_t j = 0; j < m; j++) {
-      fscanf(fp, "%lf", &v);
-      printf("%.1lf ", v);
-    }
-    printf("\n");
+    fscanf(fp, "%lf %lf %lf %lf %lf", &movie1[i], &movie2[i], &movie3[i],
+      &movie4[i], &movie5[i]);
   }
 
   /* Close file. */
   int const ret = fclose(fp);
   assert(!ret);
+
+  /* Echo contents of file. */
+  printf("%zu %zu\n", n, m);
+
+  for (size_t i = 0; i < n; i++) {
+    printf("%.1lf %.1lf %.1lf %.1lf %.1lf\n", movie1[i], movie2[i], movie3[i],
+      movie4[i], movie5[i]);
+  }
+
+  /* Deallocate memory. */
+  free(movie1);
+  free(movie2);
+  free(movie3);
+  free(movie4);
+  free(movie5);
 
   return EXIT_SUCCESS;
 }
